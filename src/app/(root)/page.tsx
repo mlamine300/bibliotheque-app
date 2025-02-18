f96d70aa25d19d736c9438d13b00281d6e276c73
lamine <laoufi.mohamed.lamine@gmail.com>
2025-02-18 14:11:48 +0100

    adding auth and database configs

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,18 @@ import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "../../../constants";
 
 import BookList from "@/components/BookList";
-export default function Home() {
+import { db } from "@/db";
+import { usersTable } from "@/db/schema";
+export default async function Home() {
+  const users = await db.insert(usersTable).values({
+    email: "aaa@gmail.com",
+    fullName: "kda",
+    password: "7845654",
+    universityCard: "124fdsf54",
+    UniversityId: 1545454,
+    status: "APPROVED",
+  });
+  console.log(JSON.stringify(users));
   const book = sampleBooks[0];
   return (
     <div className="max-w-7xl  mx-auto">
---
+++ src/db/index.ts
@@ -0,0 +1,5 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import config from "../../config";
+import { neon } from "@neondatabase/serverless";
+const sql = neon(config.env.databaseUrl);
+export const db = drizzle({ client: sql });
